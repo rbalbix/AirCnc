92bb099f5c26364b013f8ad88ad0468ec7875e3b
Ricardo Balbi <rbalbi@gmail.com>
2020-02-27 11:35:43 -0300

    fix(backend): add virtuals to Spot model and expose JSON

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -23,6 +24,7 @@ if (process.env.NODE_ENV == "production") {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 // Verify the environment
