097d8fc1b48a66fc2debf65e8734fcff0e30a8af
Ricardo Balbi <rbalbi@gmail.com>
2020-02-27 22:29:14 -0300

    feat(mobile): initial expo mobile config

---
+++ mobile/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#999',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
