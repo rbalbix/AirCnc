f8b4f38bdce4c865cd455c42017dbca746d621d3
Ricardo Balbi <rbalbi@gmail.com>
2020-02-27 23:45:18 -0300

    feat(mobile): finish Login page layout

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>Teste</Text>
-    </View>
-  );
+  return <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#999',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
