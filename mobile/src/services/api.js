f27ac8f34a93ee33dcb18aae624b8d302c0e39b7
Ricardo Balbi <rbalbi@gmail.com>
2020-03-01 22:15:35 -0300

    feat(mobile): add socket io feature

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // baseURL: 'http://192.168.1.10:3333'
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.10:3333'
+  baseURL: 'http://192.168.1.109:3333'
 });
 
 export default api;
