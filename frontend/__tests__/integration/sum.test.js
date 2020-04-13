26f49c6ec544996936b0f47788143ede19e7e15e
Ricardo Balbi <rbalbi@gmail.com>
2020-04-13 11:43:41 -0300

    feat(test): add lint-staged to run tests

---
+++ frontend/__tests__/integration/sum.test.js
@@ -0,0 +1,6 @@
+// Categoria dos testes
+describe('Experiment', () => {
+  it('should sum', async () => {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ frontend/__tests__/unit/minus.test.js
@@ -0,0 +1,5 @@
+describe('Minus', () => {
+  it('should subtract', () => {
+    expect(4 - 2).toBe(2);
+  });
+});
