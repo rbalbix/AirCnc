b7b76d4d32fbaa7d90f3c6d3f72b4a24199ea7c3
Ricardo Balbi <rbalbi@gmail.com>
2020-04-17 12:38:16 -0300

    build(backend): fix images source address

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 
-const SpotSchema = mongoose.Schema({
+const SpotSchema = mongoose.Schema(
+  {
     thumbnail: String,
     company: String,
     price: Number,
     techs: [String],
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-}, {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
     toJSON: {
-        virtuals: true,
-    }
-});
+      virtuals: true,
+    },
+  }
+);
 
 SpotSchema.virtual('thumbnail_url').get(function () {
-    return `http://192.168.1.109:3333/files/${this.thumbnail}`;
+  // return `http://192.168.1.109:3333/files/${this.thumbnail}`;
+  return `https://rb-aircnc.herokuapp.com/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
